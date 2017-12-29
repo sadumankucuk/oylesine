8c2dafe9f6a3351430c9b4a3e6028b9228008027
FatmaKaya <142804016@ogr.cbu.edu.tr>
2017-12-30 02:54:19 +0300

    Begeni Sil yapıldı

---
+++ oylesine/Models/BegeniGeriAlIstek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oylesine.Models
+{
+    public class BegeniGeriAlIstek
+    {
+        public int begeniID { get; set; }
+    }
+}
